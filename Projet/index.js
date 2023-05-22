afc1c4cfb6031f6afb2828dfc368e1c4ec551c62
hicheme ben-gaied <hicheme20@hotmail.fr>
2023-05-22 23:38:18 +0100

    modification d'un film fonctionne, maintenant faire pouvoir ajouter une film

--- Projet/index.js
+++ Projet/index.js
@@ -1,14 +1,16 @@
 import express from "express";
-import router from './routes/index.js'
-import path from 'path'
+import router from './routes/index.js';
+import path from 'path';
 import dbConnect from "./database/connexion.js";
 import methodOverride from 'method-override';
-const app = express()
 
+const app = express();
 
-dbConnect()
+dbConnect();
 app.use(methodOverride('_method'));
-app.use("/public",express.static(path.join(process.cwd(), "public")))
-app.set("view engine", "ejs")
-app.use(router)
-app.listen(8081, () => console.log("Port 8081"))
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(path.join(process.cwd(), "public")));
+app.set("view engine", "ejs");
+app.use(router);
+
+app.listen(8081, () => console.log("Port 8081"));
--- Projet/routes/index.js
+++ Projet/routes/index.js
@@ -1,8 +1,10 @@
-import {Router} from 'express'
+import express from 'express';
+import { Router } from 'express';
 import Movie from "../models/Movies.js";
 
-const router = Router()
+const router = Router();
 
+router.use(express.urlencoded({ extended: true })); // Utilisez express.urlencoded pour l'analyse du corps de la requête
 
 router.get("/", async (req, res) => {
   try {
@@ -54,6 +56,44 @@ router.delete('/films/:id', async (req, res) => {
     res.status(500).send('Une erreur est survenue lors de la suppression du film');
   }
 });
+
+router.get('/films/:id/modifier', async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) {
+      return res.status(404).send('Film non trouvé');
+    }
+
+    const templateData = {
+      title: 'Modifier le film',
+      styles: ['modifier-film.css'],
+      movie: movie,
+    };
+
+    res.status(200).render('modifier-film', templateData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Une erreur est survenue');
+  }
+});
+
+router.post('/films/:id/modifier', async (req, res) => {
+  try {
+    const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!movie) {
+      return res.status(404).send('Film non trouvé');
+    }
+    res.redirect(`/film/${movie._id}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Une erreur est survenue lors de la modification du film');
+  }
+});
+
+
+
+
+
 
 router.get("/ajouter", (req,res) => {
     res.status(200).render("ajouter-film", {title : "Ajouter un film"})
